716c1cbf41de2fa4d0d2a3ad6076c224b252569e
Gabriel <gabriel_born@estudante.sesisenai.org.br>
2025-06-12 12:07:11 -0300

    a
    mudando o src do map

---
+++ .history/components/Map_20250611100724.tsx
@@ -0,0 +1,15 @@
+export default function Map() {
+  return (
+    <section className="w-full">
+      <iframe
+        height="450"
+        style={{ border: 0, width: "100%" }}
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        title="Localização do Pet Shop"
+        className="w-full"
+      />
+    </section>
+  )
+}
