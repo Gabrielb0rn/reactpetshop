44429c23d1df033964f42136dd8ca6e97bdfc9b8
Gabriel <gabriel_born@estudante.sesisenai.org.br>
2025-06-12 12:10:11 -0300

    a
    a

---
+++ .history/components/Map_20250612120851.tsx
@@ -0,0 +1,16 @@
+export default function Map() {
+  return (
+    <section className="w-full">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3537.4602184057308!2d-48.50166942453822!3d-27.548224076274096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9527475e01a8efef%3A0x5b0f13adab8483ec!2sSENAI%20Florian%C3%B3polis%20(CTAI)!5e0!3m2!1spt-BR!2sbr!4v1749740770501!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade""
+        height="450"
+        style={{ border: 0, width: "100%" }}
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        title="Localização do Pet Shop"
+        className="w-full"
+      />
+    </section>
+  )
+}
